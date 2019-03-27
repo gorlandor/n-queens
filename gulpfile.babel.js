8d5b77649d9ec706014e5919a08c68a92e047de9
Giovanni Orlando <gorlandor@gmail.com>
2019-03-27 00:25:38 -0400

    Update to gulp@4.0.0 and workbox.
    Replaces gulp-css for gulp-clean-css.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,36 +1,35 @@
-import gulp from 'gulp';
-import babel from 'gulp-babel';
-import uglify from 'gulp-uglify';
-import concat from 'gulp-concat';
-import cssMin from 'gulp-css';
+require('@babel/register');
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const cleanCSS = require('gulp-clean-css');
 
-gulp.task('default', ['styles', 'scripts', 'sw']);
-
-gulp.task('styles', () => {
-    gulp.src('src/css/styles.css')
-        .pipe(cssMin())
+function styles() {
+    return gulp
+        .src('src/css/styles.css')
+        .pipe(cleanCSS({ compatibility: '*' }))
         .pipe(gulp.dest('dist'));
-});
+}
 
-gulp.task('scripts', () => {
-    gulp.src([
-            'src/js/Coordinate.js',
-            'src/js/NQueens.js',
-            'src/js/Board.js',
-            'src/js/app.js'
-        ])
-        .pipe(babel({
-            presets: ['es2015']
-        }))
+function scripts() {
+    return gulp
+        .src(['src/js/Coordinate.js', 'src/js/NQueens.js', 'src/js/Board.js', 'src/js/app.js'])
+        .pipe(
+            babel({
+                presets: [['@babel/preset-env']],
+            })
+        )
         .pipe(concat('bundle.js'))
         .pipe(uglify())
         .pipe(gulp.dest('dist'));
+}
 
-});
+function sw() {
+    return gulp
+        .src('./node_modules/workbox-sw/build/workbox-sw.js')
+        .pipe(concat('workbox-sw.js'))
+        .pipe(gulp.dest('./dist'));
+}
 
-gulp.task('sw', () => {
-  gulp.src('./node_modules/sw-toolbox/sw-toolbox.js')
-      .pipe(concat('sw-toolbox.js'))
-      .pipe(uglify())
-      .pipe(gulp.dest('./dist'));
-});
+gulp.task('default', gulp.series(gulp.parallel([styles, scripts]), sw));
--- sw.js
+++ sw.js
@@ -1,7 +1,9 @@
-importScripts('/dist/sw-toolbox.js');
-toolbox.options.debug = false;
-
-toolbox.precache([
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.1.1/workbox-sw.js');
+workbox.setConfig({
+    debug: false,
+});
+// Revisioned files added via a glob
+workbox.precaching.precache([
     '/n-queens/',
     'index.html',
     'manifest.json',
@@ -14,17 +16,19 @@ toolbox.precache([
     'images/icons/icon-152x152.png',
     'images/icons/icon-192x192.png',
     'images/icons/icon-384x384.png',
-    'images/icons/icon-512x512.png'
+    'images/icons/icon-512x512.png',
 ]);
-toolbox.router.get('/n-queens/', toolbox.fastest);
-toolbox.router.get('index.html', toolbox.fastest);
-toolbox.router.get('dist/styles.css', toolbox.fastest);
-toolbox.router.get('dist/bundle.js', toolbox.fastest);
-toolbox.router.get('images/icons/icon-72x72.png', toolbox.fastest);
-toolbox.router.get('images/icons/icon-96x96.png', toolbox.fastest);
-toolbox.router.get('images/icons/icon-128x128.png', toolbox.fastest);
-toolbox.router.get('images/icons/icon-144x144.png', toolbox.fastest);
-toolbox.router.get('images/icons/icon-152x152.png', toolbox.fastest);
-toolbox.router.get('images/icons/icon-192x192.png', toolbox.fastest);
-toolbox.router.get('images/icons/icon-384x384.png', toolbox.fastest);
-toolbox.router.get('images/icons/icon-152x152.png', toolbox.fastest);
+
+workbox.routing.registerRoute(new RegExp('/n-queens/'), new workbox.strategies.StaleWhileRevalidate());
+
+workbox.routing.registerRoute(new RegExp('index.html'), new workbox.strategies.StaleWhileRevalidate());
+
+workbox.routing.registerRoute(new RegExp('dist/styles.css'), new workbox.strategies.StaleWhileRevalidate());
+
+workbox.routing.registerRoute(new RegExp('dist/bundle.js'), new workbox.strategies.StaleWhileRevalidate());
+
+workbox.routing.registerRoute(new RegExp('static/manifest.json'), new workbox.strategies.StaleWhileRevalidate());
+
+workbox.routing.registerRoute(new RegExp('images/icons/.*.png'), new workbox.strategies.StaleWhileRevalidate());
+
+workbox.routing.registerRoute(new RegExp('dist/workbox-sw.js'), new workbox.strategies.StaleWhileRevalidate());
